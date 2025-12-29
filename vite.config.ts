388762dbbc7aacf78d56bf11c0fd08809520b2b9
ilbrunge <ilbrunge@gmail.com>
2025-12-29 17:36:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Fix: Casting process to any to access cwd() when Node types are not correctly inferred
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     define: {
@@ -13,6 +12,9 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 3000
+    },
+    build: {
+      outDir: 'dist'
     }
   };
 });
