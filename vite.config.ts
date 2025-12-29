d6317075bf3d83c0b8f6e35a1bd7453f888289ad
ilbrunge <ilbrunge@gmail.com>
2025-12-29 17:55:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-      'process.env.COMPANY_CODE': JSON.stringify(env.COMPANY_CODE),
-    },
-    server: {
-      port: 3000
-    },
-    build: {
-      outDir: 'dist'
+{
+  "compilerOptions": {
+    "target": "ESNext",
+    "lib": ["DOM", "DOM.Iterable", "ESNext"],
+    "module": "ESNext",
+    "skipLibCheck": true,
+    "moduleResolution": "node",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "react-jsx",
+    "strict": true,
+    "allowJs": true,
+    "esModuleInterop": true,
+    "baseUrl": ".",
+    "paths": {
+      "*": ["./*"]
     }
-  };
-});
+  },
+  "include": ["**/*.ts", "**/*.tsx"]
+}
